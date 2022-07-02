b92bc1d2b3c90c162352fd5266d0bab0d06a014f
cmchicas <carlos.chicas@gmail.com>
2022-07-02 17:25:55 -0600

    ✨ NEW FEATURE: VOTED VALID

--- src/start/Routes/voteRoutesController.js
+++ src/start/Routes/voteRoutesController.js
@@ -8,80 +8,84 @@ const { DateTime } = require('luxon');
 Campaign_Singer.hasMany(Vote);
 Vote.belongsTo(Campaign_Singer);
 
--
-    router.post('/', async (req, res) => {
-        const tokendecode = jwt_decode(req.headers.authorization);
-        const { campaignId, singerId, value } = req.body;
-        if (tokendecode.email === value) {// verificamos que quien envia la solicitud sea el mismo logeado
-            const today = DateTime.utc();
-            const campaign = await Campaign.findByPk(campaignId);
-            const endDate = DateTime.fromJSDate(campaign.endDate).setZone('utc');
-            const validateDate = endDate.diff(today);
-            const votoValid = false;
-            if (validateDate >= 0) {
+router.post('/', async (req, res) => {
+    const tokendecode = jwt_decode(req.headers.authorization);
+    const { campaignId, singerId, value } = req.body;
+    if (tokendecode.email === value) {// verificamos que quien envia la solicitud sea el mismo logeado
+        const today = DateTime.utc();
+        const campaign = await Campaign.findByPk(campaignId);
+        const endDate = DateTime.fromJSDate(campaign.endDate).setZone('utc');
+        const validateDate = endDate.diff(today);
+        var isVoteValid = true;
+        if (validateDate >= 0) {// verificamos que la campaña no haya finalizado
+            try {
                 const votesCampaign = await Campaign_Singer.findAll({
                     where: {
                         'campaignId': campaignId,
                     },
                     include: Vote
                 });
-                console.log(votesCampaign)
-                // votesCampaign.forEach(async (campaignSinger) => {
-                //     Vote.findOne({
-                //         where: {
-                //             'CampaignSingerId': campaignSinger.id,
-                //             'value': value
-                //         }
-                //     }).then(async (result) => {
-                //         result ? votoValid = false : votoValid = true;
-
-                //     }).catch(async (err) => {
-                //         res.status(500).json({ message: 'Error al registrar el voto ' + err });
-                //     });
-                // });
-                // console.log(votesCampaign);
-
-                // Vote.findOne({
-                //     where: {
-                //         'CampaignSingerId': votesNominated.id,
-                //         'value': value
-                //     }
-                // }).then(async (result) => {
-                //     if (!result) {
-                //         const voteNominated = await Campaign_Singer.findAll({
-                //             where: {
-                //                 'campaignId': campaignId,
-                //                 'singerId': singerId
-                //             }
-                //         });
-                //         const oneVote = await Vote.create({
-                //             'value': value,
-                //         });
-                //         await voteNominated.addVote(oneVote);
-                //         res.json({ message: 'Voto registrado' });
-                //     } else {
-                //         res.status(500).json({ message: 'Ya has votado' });
-                //     }
-                // }).catch(err => {
-                //     res.status(500).json({ message: 'Error al registrar el voto ' + err });
-                // });
-                res.json(votesCampaign);
-            } else {
-                await Campaign.update({
-                    'status': false
-                }, {
-                    where: {
-                        'id': campaignId
-                    }
+                votesCampaign.forEach((votesSinger) => {
+                    votesSinger.votes.forEach((vote) => {
+                        if (vote.value == value) {
+                            isVoteValid = false
+                        }
+                    });
                 });
+                if (isVoteValid) {// verificamos que el usuario no haya votado antes en esta campaña
+                    const campaignSinger = await Campaign_Singer.findOne({
+                        where: {
+                            'campaignId': campaignId,
+                            'singerId': singerId,
+                        },
+                    });
+                    Vote.create({
+                        'value': value,
+                    }).then((vote) => {
+                        campaignSinger.addVote(vote).then(() => {
+                            res.status(200).json({
+                                message: 'vote saved successfully',
+                            });
+                        })
+                            .catch((err) => {
+                                res.status(500).json({
+                                    message: 'Error registering the vote' + err,
+                                });
+                            });
+
+                    })
+                        .catch((err) => {
+                            res.status(500).json({
+                                message: 'Error registering the vote' + err,
+                            });
+                        });
+                } else {
+                    res.status(400).json({
+                        message: 'You already voted for this voting'
+                    });
+                }
+            } catch (err) {
                 res.status(500).json({
-                    message: 'El tiempo de votacion ha terminado'
+                    message: 'Error registering the vote' + err,
                 });
             }
-        } else {
-            res.status(500).json({ message: 'impossible to make the request' });
-        }
 
-    });
+        } else {
+            await Campaign.update({
+                'status': false
+            }, {
+                where: {
+                    'id': campaignId
+                }
+            });
+            res.status(500).json({
+                message: 'Voting time is over'
+            });
+        }
+    } else {
+        res.status(500).json({ message: 'impossible to make the request' });
+    }
+
+});
 
 module.exports = router;
