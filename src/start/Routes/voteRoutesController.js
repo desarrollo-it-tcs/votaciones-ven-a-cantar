686db79c314069a9ab3e1991a5ae5115998dc8b9
cmchicas <carlos.chicas@gmail.com>
2022-07-02 14:44:58 -0600

    🔨 REFACTOR:

--- src/start/Routes/voteRoutesController.js
+++ src/start/Routes/voteRoutesController.js
@@ -14,35 +14,58 @@ Vote.belongsTo(Campaign_Singer);
         const { campaignId, singerId, value } = req.body;
         if (tokendecode.email === value) {// verificamos que quien envia la solicitud sea el mismo logeado
             const today = DateTime.utc();
-            const campaign = await Campaign.findByPk(campaignId);      
+            const campaign = await Campaign.findByPk(campaignId);
             const endDate = DateTime.fromJSDate(campaign.endDate).setZone('utc');
             const validateDate = endDate.diff(today);
+            const votoValid = false;
             if (validateDate >= 0) {
-                const votesNominated = await Campaign_Singer.findOne({
+                const votesCampaign = await Campaign_Singer.findAll({
                     where: {
                         'campaignId': campaignId,
-                    }
+                    },
+                    include: Vote
                 });
-                const allVotes = await votesNominated.getVotes()
-                console.log("🚀 ~ file: voteRoutesController.js ~ line 32 ~ router.post ~ allVotes", allVotes)
-                Vote.findOne({
-                    where: {
-                        'CampaignSingerId': votesNominated.id,
-                        'value': value
-                    }
-                }).then(async (result) => {
-                    if (!result) {
-                        const oneVote = await Vote.create({
-                            'value': value,
-                        });
-                        await votesNominated.addVote(oneVote);
-                        res.json({ message: 'Voto registrado' });
-                    } else {
-                        res.status(500).json({ message: 'Ya has votado' });
-                    }
-                }).catch(err => {
-                    res.status(500).json(err);
-                });
+                console.log(votesCampaign)
+                // votesCampaign.forEach(async (campaignSinger) => {
+                //     Vote.findOne({
+                //         where: {
+                //             'CampaignSingerId': campaignSinger.id,
+                //             'value': value
+                //         }
+                //     }).then(async (result) => {
+                //         result ? votoValid = false : votoValid = true;
+
+                //     }).catch(async (err) => {
+                //         res.status(500).json({ message: 'Error al registrar el voto ' + err });
+                //     });
+                // });
+                // console.log(votesCampaign);
+
+                // Vote.findOne({
+                //     where: {
+                //         'CampaignSingerId': votesNominated.id,
+                //         'value': value
+                //     }
+                // }).then(async (result) => {
+                //     if (!result) {
+                //         const voteNominated = await Campaign_Singer.findAll({
+                //             where: {
+                //                 'campaignId': campaignId,
+                //                 'singerId': singerId
+                //             }
+                //         });
+                //         const oneVote = await Vote.create({
+                //             'value': value,
+                //         });
+                //         await voteNominated.addVote(oneVote);
+                //         res.json({ message: 'Voto registrado' });
+                //     } else {
+                //         res.status(500).json({ message: 'Ya has votado' });
+                //     }
+                // }).catch(err => {
+                //     res.status(500).json({ message: 'Error al registrar el voto ' + err });
+                // });
+                res.json(votesCampaign);
             } else {
                 await Campaign.update({
                     'status': false
@@ -56,7 +79,7 @@ Vote.belongsTo(Campaign_Singer);
                 });
             }
         } else {
-            res.status(500).json([{ message: 'impossible to make the request' }]);
+            res.status(500).json({ message: 'impossible to make the request' });
         }
 
     });
